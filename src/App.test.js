2fa949c88007ed89e995482f9b9be6d13761f1fd
yoonichul <yhc2050@gmail.com>
2023-10-10 13:33:31 +0900

    231010 fixed

--- src/App.test.js
+++ src/App.test.js
@@ -3,24 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello!!!!!!!!!/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('renders learn react link', () => {
-  render(<App />);
   const linkElement = screen.getByText(/Hello!!!!!!!!!/i);
   expect(linkElement).toBeInTheDocument();
 });
